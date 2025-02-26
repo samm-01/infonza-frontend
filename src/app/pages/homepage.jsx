c4463fd4df7a7386cfde495746e491f3ddbeb103
samm-01 <sameergarg006@gmail.com>
2025-02-26 19:59:29 +0530

    build issues

--- src/app/pages/homepage.jsx
+++ src/app/pages/homepage.jsx
@@ -1,6 +1,6 @@
-import Hero from "@/app/components/Hero";
-import Navbar from '@/app/components/navabr';
-import TechStack from '@/app/components/logoCarousel';
+import Hero from "../components/hero"
+import Navbar from '../components/navabr';
+import TechStack from '../components/logoCarousel';
 // import Stats from "@/components/Stats";
 
 export default function Home() {
